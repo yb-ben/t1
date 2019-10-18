bf9c5aab1a6fa11ce6e83743e5e85e2f692672af
Yue Yuan <2834461791@qq.com>
2019-10-18 15:05:08 +0800

    php date

---
+++ PHP常用/date/n天前n小时前.php
@@ -0,0 +1,34 @@
+<?php
+
+
+ function time_tran($the_time)
+    {
+        static $now_time;
+        if(empty($now_time)) {
+            $now_time = time();
+        }
+
+        $show_time = $the_time;
+        $dur = $now_time - $show_time;
+        if ($dur < 0) {
+            return $the_time;
+        } else {
+            if ($dur < 60) {
+                return $dur . '秒前';
+            } else {
+                if ($dur < 3600) {
+                    return floor($dur / 60) . '分钟前';
+                } else {
+                    if ($dur < 86400) {
+                        return floor($dur / 3600) . '小时前';
+                    } else {
+                        if ($dur < 259200) { // 3天内
+                            return floor($dur / 86400) . '天前';
+                        } else {
+                            return $the_time;
+                        }
+                    }
+                }
+            }
+        }
+    }
